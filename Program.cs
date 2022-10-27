f4408a488e02e20891d3919522dbb060ab4b64bf
Vadik <gloom16@mail.ru>
2022-10-27 15:34:16 +0700

    Решил задачу 50

--- Program.cs
+++ Program.cs
@@ -38,4 +38,67 @@
 
 
 
+// Задача 50
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// i = 4, j = 2 -> такого числа в массиве нет
+// i = 1, j = 2 -> 2
+
+// РЕШЕНИЕ
+
+// Console.WriteLine("Введите количество строк: ");
+// int rows = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine("Введите количество столбцов: ");
+// int columns = int.Parse(Console.ReadLine()!);
+
+// int[,] array = GetArray(rows, columns, 0, 10);
+// PrintArray(array);
+
+// int[,] GetArray(int m, int n, int minValue, int maxValue){
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++){
+//         for (int j = 0; j < n; j++){
+//             result[i,j] = new Random().Next(minValue, maxValue + 1);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] Array){
+//     for (int i = 0; i < Array.GetLength(0); i++){
+//         for (int j = 0; j < Array.GetLength(1); j++){
+//             Console.Write($"{Array[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.WriteLine("Введите координаты в строках: ");
+// int a = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine("Введите координаты в столбцах: ");
+// int b = int.Parse(Console.ReadLine()!);
+
+
+// for (int i = 0; i < array.GetLength(0); i++){
+//     for (int j = 0; j < array.GetLength(1); j++){
+//         if (a == i && b == j){
+//             Console.WriteLine($"Значение заданного элемента = {array[i,j]}");
+//         }
+//         if (a > array.GetLength(0) || b > array.GetLength(1) ){
+//             Console.WriteLine("такого числа нет!!!");
+//         }
+//     }
+// }
+
+
+
+
+
+
 
