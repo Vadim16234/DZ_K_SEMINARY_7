1201a77d8ebb32850dfe30ecfd362af5568b3547
Vadik <gloom16@mail.ru>
2022-10-27 17:36:16 +0700

    Решил задачу 52

--- Program.cs
+++ Program.cs
@@ -99,6 +99,57 @@
 
 
 
+// Задача 52
 
+// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
+// РЕШЕНИЕ
 
+// Console.WriteLine("Введите количество строк: ");
+// int rows = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine("Введите количество столбцов: ");
+// int columns = int.Parse(Console.ReadLine()!);
+
+// int[,] array = GetArray(rows, columns, 0, 10);
+// PrintArray(array);
+// GlobalArray(array);
+
+// int[,] GetArray(int m, int n, int minValue, int maxValue ){
+//     int[,] result = new int[m,n];
+//     for (int i = 0; i < m; i++){
+//         for (int j = 0; j < n; j++){
+//             result[i,j] = new Random().Next(minValue, maxValue + 1); 
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] Array){
+//     for (int i = 0; i < Array.GetLength(0); i++){
+//         for (int j = 0; j < Array.GetLength(1); j++){
+//             Console.Write($"{Array[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+//  void GlobalArray(int[,] myArray)
+//  {
+//      double[] result = new double[myArray.GetLength(1)];
+//         for (int j = 0; j < myArray.GetLength(1); j++)
+//      {
+//          double sum = 0;
+//          for (int i = 0; i < myArray.GetLength(0); i++)
+//          {
+//              sum = sum + myArray[i, j];
+//          }
+//          result[j] = sum / myArray.GetLength(0);
+//      }
+//      Console.WriteLine($"{String.Join(" | ", result)}");
+//  }
